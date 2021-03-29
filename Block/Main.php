13401f68af14d4543217a92cb07e097fd0bfbfae
devops-user@rapyd.net <jenkins@jenkins.com>
2021-03-29 08:38:32 +0000

    pushed from bitbucket

--- Block/Main.php
+++ Block/Main.php
@@ -52,43 +52,34 @@ class Main extends \Magento\Framework\View\Element\Template
                 $shipping = $order->getShippingAddress();
                 $base_url = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB);
                 $body = [
-                'amount' => $this->encode_string($order->getGrandTotal()),
-                'currency_code' => $this->encode_string($order->getGlobalCurrencyCode()),
-                'reference_id' => $this->encode_string($orderId),
-                'user_id' => $this->encode_string($this->customerSession->getCustomer()->getId()),
-                'webhook_url' => $this->encode_string($base_url . 'rest/V1/rapyd/webhook/'),
-                'refund_url' => $this->encode_string($base_url . 'rest/V1/rapyd/refund/'),
-                'complete_payment_url' => $this->encode_string($base_url . 'rapyd/success/'),
-                'error_payment_url' =>$this->encode_string($base_url . 'rapyd/success/'),
-                'cancel_checkout_url'=> $this->encode_string($base_url . 'rapyd/success/'),
-                'order_id' => $this->encode_string($orderId),
-                'receipt_email' => $this->encode_string($billing->getEmail()),
-                'country_code' => $this->encode_string($billing->getCountryId()),
-                'customer_first_name' =>$this->encode_string($billing->getFirstname()),
-                'customer_last_name' => $this->encode_string($billing->getLastname()),
-                'customer_phone' => $this->encode_string($billing->getTelephone()),
-                'shipping_address' => [
-                    'line1' => $this->encode_string($shipping->getStreet()[0]),
-                    'line2' => $this->getLine2($shipping),
-                    'city' => $this->encode_string($shipping->getCity()),
-                    'state' =>$this->encode_string($shipping->getRegion()),
-                    'phone_number' => $this->encode_string($shipping->getTelephone()),
-                    'country' => $this->encode_string($shipping->getCountryId()),
-                    'zip' => $this->encode_string($shipping->getPostcode())
-                ],
-                'billing_address'=>[
-                    'line1' => $this->encode_string($billing->getStreet()[0]),
-                    'line2' => $this->getLine2($billing),
-                    'city' => $this->encode_string($billing->getCity()),
-                    'state' =>$this->encode_string($billing->getRegion()),
-                    'phone_number' => $this->encode_string($billing->getTelephone()),
-                    'country' => $this->encode_string($billing->getCountryId()),
-                    'zip' => $this->encode_string($billing->getPostcode())
-                ],
-                'category' => $this->encode_string($order->getPayment()->getMethodInstance()->getCode()),
-                'cart' => $this->encode_string($this->getCartItems($order))
-            ];
-
+                            'amount' => $this->encode_string($order->getGrandTotal()),
+                            'currency_code' => $this->encode_string($order->getGlobalCurrencyCode()),
+                            'reference_id' => $this->encode_string($orderId),
+                            'user_id' => $this->encode_string($this->customerSession->getCustomer()->getId()),
+                            'webhook_url' => $this->encode_string($base_url . 'rest/V1/rapyd/webhook/'),
+                            'refund_url' => $this->encode_string($base_url . 'rest/V1/rapyd/refund/'),
+                            'complete_payment_url' => $this->encode_string($base_url . 'rapyd/success/'),
+                            'error_payment_url' =>$this->encode_string($base_url . 'rapyd/success/'),
+                            'cancel_checkout_url'=> $this->encode_string($base_url . 'rapyd/success/'),
+                            'order_id' => $this->encode_string($orderId),
+                            'receipt_email' => $this->encode_string($billing->getEmail()),
+                            'country_code' => $this->encode_string($billing->getCountryId()),
+                            'customer_first_name' =>$this->encode_string($billing->getFirstname()),
+                            'customer_last_name' => $this->encode_string($billing->getLastname()),
+                            'customer_phone' => $this->encode_string($billing->getTelephone()),
+                            'shipping_address'=> $this->getShippingDetails($shipping),
+                            'billing_address'=>[
+                                'line1' => $this->encode_string($billing->getStreet()[0]),
+                                'line2' => $this->getLine2($billing),
+                                'city' => $this->encode_string($billing->getCity()),
+                                'state' =>$this->encode_string($billing->getRegion()),
+                                'phone_number' => $this->encode_string($billing->getTelephone()),
+                                'country' => $this->encode_string($billing->getCountryId()),
+                                'zip' => $this->encode_string($billing->getPostcode())
+                            ],
+                            'category' => $this->encode_string($order->getPayment()->getMethodInstance()->getCode()),
+                            'cart' => $this->encode_string($this->getCartItems($order))
+                        ];
                 $order->save();
 
                 $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
@@ -132,6 +123,21 @@ class Main extends \Magento\Framework\View\Element\Template
             //$this->_messageManager->addErrorMessage($e->getMessage());
             $this->_messageManager->addErrorMessage('An error occurred. Please try again.');
         }
+    }
+
+    public function getShippingDetails($shipping){
+        if($shipping){
+            return [
+                'line1' => $this->encode_string($shipping->getStreet()[0]),
+                'line2' => $this->getLine2($shipping),
+                'city' => $this->encode_string($shipping->getCity()),
+                'state' =>$this->encode_string($shipping->getRegion()),
+                'phone_number' => $this->encode_string($shipping->getTelephone()),
+                'country' => $this->encode_string($shipping->getCountryId()),
+                'zip' => $this->encode_string($shipping->getPostcode())
+            ];
+        }
+        return ['NO_DATA' =>$this->encode_string('NO_DATA')];
     }
 
     public function getCartItems($order)
